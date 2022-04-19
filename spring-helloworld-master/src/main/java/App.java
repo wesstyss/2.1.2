8c4f20530b1be60c913b9e6886727d7c2282ec53
Ruslan Hairullin <kh.ruslan00@mail.ru>
2022-04-19 18:56:35 +0500

    Initial commit

---
+++ spring-helloworld-master/src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld "+(bean==bean1));
+        System.out.println("Cat "+(cat==cat1));
+    }
+}
